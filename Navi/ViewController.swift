63b08fcdffdc0431ac60ddd45dd38e55d4b5591c
Susan Kim <suzenyada@gmail.com>
2021-07-28 15:55:02 +0900

    Initial Commit

---
+++ Navi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navi
+//
+//  Created by Susan Kim on 2021/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
