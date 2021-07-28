c0dc031ed16b2ad25dafbb24cf96a20f8c2bb467
Susan Kim <suzenyada@gmail.com>
2021-07-28 17:46:04 +0900

    Added Quiz ViewController
    dd

---
+++ Navi/QuizVC.swift
@@ -0,0 +1,18 @@
+//
+//  QuizVC.swift
+//  Navi
+//
+//  Created by Susan Kim on 2021/07/28.
+//
+
+import UIKit
+
+class QuizVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+
+}
--- Navi/TabBarController.swift
+++ Navi/TabBarController.swift
@@ -15,7 +15,7 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let colors: [UIColor] = [.blue, .red, .orange, .purple, .green]
+        let colors: [UIColor] = [.blue, .orange, .purple]
         var viewControllers = colors.map { color -> UIViewController in
             let vc = ViewController()
             vc.view.backgroundColor = color
@@ -24,11 +24,16 @@ class TabBarController: UITabBarController {
             return navContr
         }
         
+        let quizVC = QuizVC()
+        let quizNavController = UINavigationController(rootViewController: quizVC)
+        quizNavController.title = "Quiz"
+        
         let homeVC = HomeVC()
         let homeNavController = UINavigationController(rootViewController: homeVC)
         homeNavController.title = "Home"
         
-        viewControllers[2] = homeNavController
+        viewControllers[0] = quizNavController
+        viewControllers[1] = homeNavController
         
         self.viewControllers = viewControllers
     }
