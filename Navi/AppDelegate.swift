c4a75e5936b65a2925c280241ae57c6f7ecace03
Susan Kim <suzenyada@gmail.com>
2021-07-28 16:44:48 +0900

    Create window and attach initial view controller

--- Navi/AppDelegate.swift
+++ Navi/AppDelegate.swift
@@ -10,10 +10,18 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        if #available(iOS 13.0, *) {
+            // do nothing we will have code in SceneDelegate for this
+        } else {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = ViewController()
+            window?.makeKeyAndVisible()
+        }
+        
         return true
     }
 
