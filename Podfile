5f16c9bb6c555e54c69285e7836afa4ea072c899
Susan Kim <suzenyada@gmail.com>
2021-10-20 18:49:53 +0900

    Delete unused pod: PanModal

--- Podfile
+++ Podfile
@@ -10,7 +10,6 @@ target 'Navi' do
 
   # Pods for Navi
   pod 'SnapKit', '~> 5.0.0'
-  pod 'PanModal'
   pod 'SkyFloatingLabelTextField', '~> 3.0'
 
   target 'NaviTests' do
--- Pods/Target Support Files/Pods-Navi/Pods-Navi-frameworks.sh
+++ Pods/Target Support Files/Pods-Navi/Pods-Navi-frameworks.sh
@@ -175,12 +175,10 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/PanModal/PanModal.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/PanModal/PanModal.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SkyFloatingLabelTextField/SkyFloatingLabelTextField.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
