35bc32b375dd3eed6268253b0a1c4b30b35dfc6a
Susan Kim <suzenyada@gmail.com>
2021-10-22 14:22:24 +0900

    Update to contain memorize status as numbers

---
+++ Navi/MyPage/View/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  Navi
+//
+//  Created by Susan Kim on 2021/10/22.
+//
+
+import UIKit
+
+class ChartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Navi/MyPage/ViewModel/MemorizeStatusViewModel.swift
+++ Navi/MyPage/ViewModel/MemorizeStatusViewModel.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 struct MemorizeStatusViewModel {
-    let statuses: [(title: String, content: String)]
+    let status: [(title: String, content: String)]
+    let memorizeStatus: [(theme: String, memorized: Int, total: Int)]
     
     init() {
         let verses = DataBaseService.shared.fetch(request: Verse.fetchRequest())
@@ -16,9 +17,18 @@ struct MemorizeStatusViewModel {
         let oyoVerses = DataBaseService.shared.fetch(request: Verse.fetchRequestOfOYO())
         let oyoMemCount = oyoVerses.filter { $0.memorized == true }.count
         
-        statuses = [
+        status = [
             ("외운 카드 수 / 모든 카드 수", "\(memCount) / \(verses.count)개 "),
             ("외운 카드 수 / OYO 카드 수", "\(oyoMemCount) / \(oyoVerses.count)개 ")
         ]
+        
+        var memStatus = [(theme: String, memorized: Int, total: Int)]()
+        DataBaseService.shared.themes.forEach { theme in
+            let verses = DataBaseService.shared.fetch(request: Verse.fetchRequest(of: theme))
+            let total = verses.count
+            let memorized = verses.filter{ $0.memorized }.count
+            memStatus.append((theme, memorized, total))
+        }
+        memorizeStatus = memStatus
     }
 }
