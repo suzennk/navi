4192eb69540dfd2af883a87aace6b7b145322f9d
Susan Kim <suzenyada@gmail.com>
2021-10-27 14:45:23 +0900

    Update colors

--- Navi/Extension/UIColor+Extension.swift
+++ Navi/Extension/UIColor+Extension.swift
@@ -9,15 +9,12 @@ import UIKit
 
 extension UIColor {
     static let naviYellow = UIColor(red: 255/255.0, green: 192/255.0, blue: 0/255.0, alpha: 1.0)
-    static let headerYellow = UIColor(named: "yellow-header")
     
-    static let selectedBackground = UIColor(red: 247/255.0, green: 203/255.0, blue: 34/255.0, alpha: 1.0)
+    static let selectedBackground = UIColor.naviYellow
     static let unselectedBackground = UIColor.systemGray5
     
     static let selectedText = UIColor.black
     static let unselectedText = UIColor.systemGray
     
     static let borderColor = UIColor(red: 112/255.0, green: 112/255.0, blue: 112/255.0, alpha: 1.0)
-    
-    static let navyText = UIColor(red: 62/255.0, green: 65/255.0, blue: 128/255.0, alpha: 1.0)
 }
