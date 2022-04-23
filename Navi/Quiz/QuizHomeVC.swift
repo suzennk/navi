69050695b9e85c30fcde5508b8e20c4bf858fe3e
Susan Kim <suzenyada@gmail.com>
2022-04-23 18:07:12 +0900

    Feat: Implemented quiz solves...

--- Navi/Quiz/QuizHomeVC.swift
+++ Navi/Quiz/QuizHomeVC.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 
-class QuizHomeVC: UIViewController {
-
+class QuizHomeVC: ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let quizeRangeVC = segue.destination as? QuizRangeCollectionVC else { return }
+        if segue.identifier == "bibleRangeSegue" {
+            quizeRangeVC.quizType = .bibleRange
+        } else if segue.identifier == "titleSegue" {
+            quizeRangeVC.quizType = .title
+        } else if segue.identifier == "contentSegue" {
+            quizeRangeVC.quizType = .content
+        }
+    }
 }
